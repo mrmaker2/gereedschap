b726b155cda7d647e9b33635ee31f1f941efbde3
andres linclau <andres.linclau@gmail.com>
2019-06-19 20:14:50 +0200

    sign up pagina aangepast

--- tools/lib/SignUpPage.dart
+++ tools/lib/SignUpPage.dart
@@ -11,25 +11,53 @@ class _SignUpPageState extends State<SignUpPage> {
     return Scaffold(
       body: Column(
         children: <Widget>[
-          Form(
-            child: Column(
-              children: <Widget>[
-                TextFormField(
-                  autocorrect: true,
-                  decoration: InputDecoration(
-                      alignLabelWithHint: true, labelText: "Email"),
-                ),
-                TextFormField(
-                  autocorrect: true,
-                  decoration: InputDecoration(
-                      alignLabelWithHint: true, labelText: "Email"),
-                ),
-                RaisedButton(
-                  onPressed: () => debugPrint("SignUp"),
+          Column(
+            children: <Widget>[
+              Align(
+                alignment: Alignment.topCenter,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 60.0),
                   child: Text("Sign Up"),
-                )
-              ],
-            ),
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(32.0),
+                child: Form(
+                  child: Column(
+                    children: <Widget>[
+                      TextFormField(
+                        autocorrect: true,
+                        decoration: InputDecoration(
+                            alignLabelWithHint: true, labelText: "Email"),
+                      ),
+                      TextFormField(
+                        autocorrect: true,
+                        decoration: InputDecoration(
+                            alignLabelWithHint: true, labelText: "password"),
+                      ),
+                      SizedBox(
+                        height: 30.0,
+                      ),
+                      RaisedButton(
+                        onPressed: () => debugPrint("SignUp"),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 12.0, horizontal: 24.0),
+                          child: Text(
+                            "Sign Up",
+                            style: TextStyle(
+                                fontSize: 25.0, fontWeight: FontWeight.w900),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
           )
         ],
       ),
